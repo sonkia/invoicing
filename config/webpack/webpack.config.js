3c1111d8acdd1425fe7bfd71381c05b05d189a5a
songjia <finers@qq.com>
2019-02-16 17:05:48 +0800

    webpack config

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 module.exports = {
-    entry: path.resolve(__dirname, '../src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
+    entry: path.resolve(__dirname, '../../src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
     output: {
-        path: path.resolve(__dirname, '../dist'), // 输出的路径
+        path: path.resolve(__dirname, '../../dist'), // 输出的路径
         filename: 'bundle.js'  // 打包后文件
     },
     module:{
